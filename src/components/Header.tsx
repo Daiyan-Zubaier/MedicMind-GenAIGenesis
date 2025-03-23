7783c0588b185297fe5b49a0748ffda4b020d0cd
Arushan Arulraj <arushanarul13@gmail.com>
2025-03-23 11:58:56 -0400

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ const Header: React.FC = () => {
         <div className="flex items-center space-x-3">
           <Heart className="h-6 w-6 text-emergency" />
           <h1 className="text-2xl font-light tracking-tight">
-            <span className="font-semibold">AI</span> First-Aid Guide
+            <span className="font-semibold">MedicMind</span>
           </h1>
         </div>
       </div>
